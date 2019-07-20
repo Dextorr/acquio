5782ecc3a0930607d3470b172c9a0e3e082ab5e9
Dexter De Leon <dexdeleon@gmail.com>
2019-07-21 00:39:47 +0100

    Browser Router set up with react-router-dom. Navbar component rendered. Material UI installed.

--- src/app.js
+++ src/app.js
@@ -1,12 +1,21 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
+
+import Home from './components/Home'
+import Navbar from './components/common/Navbar'
 
 import './style.scss'
 
 class App extends React.Component {
   render(){
     return(
-      <h1>Acquio Recruitment</h1>
+      <BrowserRouter>
+        <Navbar />
+        <Switch>
+          <Route path='/' component={Home} />
+        </Switch>
+      </BrowserRouter>
     )
   }
 }
---
+++ src/components/common/Navbar.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import { AppBar, Toolbar } from '@material-ui/core'
+
+const Navbar = () => {
+  return (
+    <AppBar position="static">
+      <Toolbar>
+
+      </Toolbar>
+    </AppBar>
+  )
+}
+
+export default Navbar
