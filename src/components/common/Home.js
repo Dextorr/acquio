17bf97d330c18166b77eb725d937a889b2000a79
Dexter De Leon <dexdeleon@gmail.com>
2019-08-07 18:49:37 +0100

    Hero div added to Home, image assets added.

--- src/components/common/Home.js
+++ src/components/common/Home.js
@@ -6,21 +6,23 @@ import { Link } from 'react-router-dom'
 const Home = () => {
   return(
     <main className="home">
-      <Container>
+      <div className="hero">
+        <Container>
 
-        <Link to="/hiring">
-          <Button>
-            HIRING
-          </Button>
-        </Link>
+          <Link to="/hiring">
+            <Button>
+              HIRING
+            </Button>
+          </Link>
 
-        <Link to="/jobs">
-          <Button>
-            JOBS
-          </Button>
-        </Link>
-        
-      </Container>
+          <Link to="/jobs">
+            <Button>
+              JOBS
+            </Button>
+          </Link>
+
+        </Container>
+      </div>
     </main>
   )
 }
