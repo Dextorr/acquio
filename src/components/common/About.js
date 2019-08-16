72a42cc3b5d3ec4ebe4139301bba5ee023b52931
Dexter De Leon <dexdeleon@gmail.com>
2019-08-16 16:37:10 +0100

    Home component made into a classical component for storing recent jobs. About component overlay added.

--- src/components/common/About.js
+++ src/components/common/About.js
@@ -6,8 +6,12 @@ const About = () => {
   return (
     <section className="about">
       <div className="about-overlay">
+        <Header as="h2">
+          <Container>
+            About Us
+          </Container>
+        </Header>
         <Container>
-          <Header as="h2">About Us</Header>
           <div className="about-content">
             <p>
               Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
--- src/components/common/Home.js
+++ src/components/common/Home.js
@@ -1,15 +1,31 @@
 import React from 'react'
+import axios from 'axios'
 
 import Hero from './Hero'
 import About from './About'
+import RecentJobs from '../jobs/RecentJobs'
 
-const Home = () => {
-  return(
-    <main className="home">
-      <Hero />
-      <About />
-    </main>
-  )
+class Home extends React.Component {
+
+  componentDidMount(){
+    axios.get('/api/jobs')
+      .then(res => {
+        const jobs = res.data.filter((job, i) => i < 4)
+        this.setState({ jobs })
+      })
+      .catch(err => console.error(err))
+  }
+
+  render(){
+    if(!this.state) return null
+    return(
+      <main className="home">
+        <Hero />
+        <About />
+        <RecentJobs jobs={this.state.jobs} />
+      </main>
+    )
+  }
 }
 
 export default Home
