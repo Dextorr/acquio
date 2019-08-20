b5dfba3fba159e727b085f81916e1e42a54f8985
Dexter De Leon <dexdeleon@gmail.com>
2019-08-20 13:35:34 +0100

    Added sort function to recent jobs to show most recently posted jobs.

--- src/components/common/Home.js
+++ src/components/common/Home.js
@@ -10,7 +10,11 @@ class Home extends React.Component {
   componentDidMount(){
     axios.get('/api/jobs')
       .then(res => {
-        const jobs = res.data.filter((job, i) => job.active && i < 4)
+        const jobs = res.data.sort((a, b) => {
+          const aDate = new Date(a.createdAt)
+          const bDate = new Date(b.createdAt)
+          return bDate - aDate
+        }).filter((job, i) => job.active && i < 4)
         this.setState({ jobs })
       })
       .catch(err => console.error(err))
