96874aa899147fc9e652f52bc13793e90fc94b74
Dexter De Leon <dexdeleon@gmail.com>
2019-08-25 19:21:56 +0100

    JobsShow component updated, functions added to apply/quick apply buttons.

--- src/components/jobs/JobsShow.js
+++ src/components/jobs/JobsShow.js
@@ -6,11 +6,27 @@ import { Container, Grid, Segment, Button } from 'semantic-ui-react'
 import Auth from '../../lib/Auth'
 
 class JobsShow extends React.Component {
+  constructor(){
+    super()
+
+    this.apply = this.apply.bind(this)
+    this.quickApply = this.quickApply.bind(this)
+  }
 
   componentDidMount(){
     axios.get(`/api/jobs/${this.props.match.params.id}`)
       .then(res => this.setState({ job: res.data }))
       .catch(err => console.error(err))
+  }
+
+  apply(){
+    axios.put(`/api/jobs/${this.state.job._id}`)
+      .then(() => this.setState({ message: `Thanks for applying for the ${this.state.job.name} role!` }))
+      .catch(err => this.setState({ error: err.response.data }))
+  }
+
+  quickApply(){
+    this.props.history.push(`/quickApply/${this.state.job._id}`)
   }
 
   render(){
@@ -20,24 +36,32 @@ class JobsShow extends React.Component {
       <main className="job">
         <Container>
           <Segment>
-            <h2>{job.name}</h2>
-            <p>{job.description}</p>
-          </Segment>
+            <Grid stackable>
 
-          <Segment>
-            <Grid stackable className="sectors">
-              {job.sectors.map(sector =>
-                <Grid.Column key={sector._id} computer={6}>
-                  {sector.name}
-                </Grid.Column>
-              )}
+              <Grid.Column computer={10}>
+                <h2>{job.name}</h2>
+                <p>{job.description}</p>
+              </Grid.Column>
+
+              <Grid.Column computer={6}>
+
+                <Grid stackable className="sectors">
+                  {job.sectors.map(sector =>
+                    <Grid.Column key={sector._id} computer={8}>
+                      <span>{sector.name}</span>
+                    </Grid.Column>
+                  )}
+                </Grid>
+
+              </Grid.Column>
+
             </Grid>
           </Segment>
 
           {Auth.isAuthenticated() ?
-            <Button>Apply</Button>
+            <Button onClick={this.apply}>Apply</Button>
             :
-            <Button>Quick Apply</Button>
+            <Button onClick={this.quickApply}>Quick Apply</Button>
           }
 
         </Container>
